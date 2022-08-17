153312b2d58ca59dd0ea461d04d5584bab6d9813
wejdenhadded <wejden.hadded@farkito.tn>
2022-08-17 19:08:38 +0100

    add accordion component to app

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-      <nav className=" shadow-sm fixed w-full z-10">
+      <nav className=" shadow-sm absolute w-full z-10">
         <div className="w-full">
           <div className="flex items-center h-20 w-full">
             <div className="flex items-center  mx-20  justify-between w-full ">
